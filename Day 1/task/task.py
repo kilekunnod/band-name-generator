226dd2b81e74caadb0435d5e24ada93b16d4b442
Kilekun <kilekunonajide@gmail.com>
2024-09-11 16:55:06 +0100

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,7 @@
+print("Hello, welcome to the Band Name Generator!")
+# variable 'city' to store the user's input to the question
+city = input("What's the name of the city you grew up in?\n")
+# variable 'pet' to store the user's input to the question
+pet = input("What's your pet's name?\n")
+# output of both variables
+print("Your band's name could be: " + city + " " + pet + "!")
