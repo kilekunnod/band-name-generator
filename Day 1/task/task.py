9ac721681ab426473387080e6a72d0afdbd5005d
Kilekun <kilekunonajide@gmail.com>
2024-09-11 17:01:01 +0100

    day 1 of dr. angela yu's python bootcamp

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -4,4 +4,4 @@ city = input("What's the name of the city you grew up in?\n")
 # variable 'pet' to store the user's input to the question
 pet = input("What's your pet's name?\n")
 # output of both variables
-print("Your band's name could be: " + city + " " + pet + "!")
+print("Your band's name could be: " + city + " " + pet)
